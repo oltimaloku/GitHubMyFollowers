583c7f114ee71ed6d6c93cff2ad0857cecb5961a
oltimaloku <oltimaloku555@gmail.com>
2024-09-17 19:04:06 -0700

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Olti Maloku on 2024-09-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
